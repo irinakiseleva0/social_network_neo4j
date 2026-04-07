2f67ca562b18836ff6f076e5cb7f6019b716f25d
irinakiseleva0 <irina.kiseleva@epita.fr>
2026-04-07 18:20:05 +0200

    Clean up Neo4j migration script configuration

--- migrate_sqlite_to_neo4j.py
+++ migrate_sqlite_to_neo4j.py
@@ -6,7 +6,7 @@ from neo4j import GraphDatabase
 SQLITE_DB = "social_network.db"
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
 NEO4J_USERNAME = os.getenv("NEO4J_USERNAME", "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "Neo4jPass123")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password")
 NEO4J_DATABASE = os.getenv("NEO4J_DATABASE", "neo4j")
 
 
